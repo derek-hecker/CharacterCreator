450bd17464f095bcec11517dd2baca6e1b48dfb3
derek hecker <derekhecker8@gmail.com>
2020-08-01 16:32:36 -0400

    Initial Commit - general form outline

--- CharacterCreator/Program.cs
+++ CharacterCreator/Program.cs
@@ -17,6 +17,7 @@ namespace CharacterCreator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            DndCharacter Player = new DndCharacter();
         }
     }
 }
