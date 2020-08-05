c54708658cc9a50da7f01a1b4ec491a5d2e69ebb
derek hecker <derekhecker8@gmail.com>
2020-08-05 17:57:25 -0400

    Final Commit before submission

--- CharacterCreator/Noble.cs
+++ CharacterCreator/Noble.cs
@@ -13,7 +13,7 @@ namespace CharacterCreator
             Description = "You carry a noble title, and your family owns land, collects taxes, and wields significant political influence. You might be a pampered aristocrat unfamiliar with work or discomfort, a former merchant just elevated to the nobility, or a disinherited scoundrel with a disproportionate sense of entitlement.";
             Feature = "Position of Privelege";
             Extra = "One Language of your choice";
-            Proficiencies = "History, Persuasion";
+            Proficiencies = "History, Persuasion"; 
         }
     }
 }
